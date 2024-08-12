9a8e4c82be1e45dc90c288bf7ae3fffe6df4f442
Administrateur <nourri.jer@gmail.com>
2024-08-12 11:29:01 +0200

    exercice 1 aspect fix

--- src/main/java/com/example/exercice1/aspect/LoggingAspect.java
+++ src/main/java/com/example/exercice1/aspect/LoggingAspect.java
@@ -3,6 +3,7 @@ package com.example.exercice1.aspect;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -20,10 +21,17 @@ public class LoggingAspect {
 
     @After("execution(* com.example.exercice1.service.BookService.*(..))")
     public void logAfter(ProceedingJoinPoint proceedingJoinPoint) {
-        System.out.println("Méthode terminée");
+        System.out.println("After for " + proceedingJoinPoint.getSignature().getName());
         Object[] args = proceedingJoinPoint.getArgs();
         System.out.println(args);
 
+    }
+
+    @AfterReturning("execution(* com.example.exercice1.service.BookService.*(..))")
+    public void logAfterReturning(ProceedingJoinPoint proceedingJoinPoint) {
+        System.out.println("Returning for " + proceedingJoinPoint.getSignature().getName());
+        Object[] args = proceedingJoinPoint.getArgs();
+        System.out.println(args);
 
     }
 
